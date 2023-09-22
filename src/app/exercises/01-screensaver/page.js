04337bae58cc1a5fe78f737efe5106bfc5c2e9ba
László Czöndör <czondi@hotmail.com>
2023-09-22 21:45:13 +0200

    feat(screensaver): choose screensaver color from list

--- src/app/exercises/01-screensaver/page.js
+++ src/app/exercises/01-screensaver/page.js
@@ -1,11 +1,28 @@
-import React from 'react';
-
-import ScreenSaver from '../../../components/ScreenSaver';
+import React from "react";
+import Link from "next/link";
+import COLORS from "./css_colors";
 
 function ScreenSaverExercise() {
   return (
-    <main className="screen-saver-wrapper">
-      <ScreenSaver color="red" />
+    <main>
+      <p>Choose your color:</p>
+      <ul>
+        {Object.entries(COLORS).map(([color]) => {
+          const backgroundColor = color === "Black" ? "white" : "unset";
+          const style = {
+            color,
+            backgroundColor,
+          };
+
+          return (
+            <li key={color} style={{ color }}>
+              <Link href={`01-screensaver/${color}`} style={style}>
+                {color}
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
     </main>
   );
 }
